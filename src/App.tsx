6a63460ee7d0685c3dfb02726986b67e0de12766
7evendays <codingkirby0@gmail.com>
2024-12-31 22:24:18 +0900

    feat: 기본 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./api/queryClient";
+
 import { AppThemeProvider } from "./context/themeContext";
 import Home from "./pages/Home";
 
+const routeList = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
+
+const router = createBrowserRouter(
+  routeList.map((item) => {
+    return {
+      ...item,
+      element: item.element,
+    };
+  })
+);
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AppThemeProvider>
-        <Home />
+        <RouterProvider router={router} />
       </AppThemeProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 

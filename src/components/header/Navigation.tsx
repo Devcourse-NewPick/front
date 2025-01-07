f4252bfddf48ee8508dfc0111c930f92bf0669fa
KIMJEONGHYEON <68999618+CodingKirby@users.noreply.github.com>
2025-01-07 20:11:28 +0900

    chore: 네비게이션 메뉴명 수정

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -1,63 +1,64 @@
-import { styled } from "styled-components";
-import { NAVIGATION } from "../../constants/navigation";
+import { styled } from 'styled-components';
+import { NAVIGATION } from '../../constants/navigation';
 
 const Navigation = () => {
-  return (
-    <StyledNavigation id="navigation">
-      <ul>
-        {NAVIGATION.map((item, index) => (
-          <li key={index}>{item.title}</li>
-        ))}
-      </ul>
-    </StyledNavigation>
-  );
+	return (
+		<StyledNavigation id="navigation">
+			<ul>
+				{NAVIGATION.map((item, index) => (
+					<li key={index}>{item.title}</li>
+				))}
+			</ul>
+		</StyledNavigation>
+	);
 };
 
 const StyledNavigation = styled.nav`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  white-space: nowrap;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-start;
+	white-space: nowrap;
 
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 
-  ul {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-    list-style: none;
-    padding: 0;
-    margin: 0 1rem 0 1rem;
-    gap: 1.2rem;
-    line-height: 1;
+	ul {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: flex-start;
+		list-style: none;
+		padding: 0;
+		margin: 0 1rem 0 1rem;
+		gap: 1.2rem;
+		line-height: 1;
 
-    li {
-      padding: 0;
-      margin: 0;
-      cursor: pointer;
+		li {
+			padding: 0;
+			margin: 0;
+			cursor: pointer;
 
-      font-size: ${({ theme }) => theme.fontSize.small};
+			font-size: ${({ theme }) => theme.fontSize.small};
 
-      &:hover {
-        color: ${({ theme }) => theme.color.primary};
-      }
+			&:hover {
+				color: ${({ theme }) => theme.color.primary};
+				font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+			}
 
-      a {
-        font-size: ${({ theme }) => theme.fontSize.medium};
-        font-weight: ${({ theme }) => theme.fontWeight.medium};
-        color: ${({ theme }) => theme.color.text};
-        text-decoration: none;
-        transition: color 0.3s ease;
-      }
-    }
-  }
+			a {
+				font-size: ${({ theme }) => theme.fontSize.medium};
+				font-weight: ${({ theme }) => theme.fontWeight.medium};
+				color: ${({ theme }) => theme.color.text};
+				text-decoration: none;
+				transition: color 0.3s ease;
+			}
+		}
+	}
 
-  @media ${({ theme }) => theme.mediaQuery.mobile} {
-    display: none;
-  }
+	@media ${({ theme }) => theme.mediaQuery.mobile} {
+		display: none;
+	}
 `;
 
 export default Navigation;

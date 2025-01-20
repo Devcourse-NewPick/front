b05ffb23a91d89f305e20289f8a7c40a99c2522a
KIMJEONGHYEON <68999618+CodingKirby@users.noreply.github.com>
2025-01-20 13:41:10 +0900

    fix: turbopack 활성화

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	distDir: 'build',
-
-	// Webpack 설정
-	webpack: (config) => {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			use: [
-				{
-					loader: '@svgr/webpack',
-					options: {
-						svgoConfig: {
-							plugins: [{ removeViewBox: false }],
-						},
-					},
+	experimental: {
+		turbo: {
+			rules: {
+				'*.svg': {
+					loaders: ['@svgr/webpack'],
+					as: '*.js',
 				},
-			],
-		});
-		return config;
+			},
+		},
 	},
-
 	// 이미지 설정
 	images: {
 		remotePatterns: [
@@ -31,11 +21,7 @@ const nextConfig: NextConfig = {
 				pathname: '/**',
 			},
 		],
-	},
-
-	// 스타일 컴포넌트 설정
-	compiler: {
-		styledComponents: true,
+		domains: ['picsum.photos'],
 	},
 };
 

285ad512ada42cb31bba51c38349c9133d409e49
Jay.Hyeon <hyinky@gmail.com>
2025-01-21 23:18:36 +0900

    fix: build 파일 추가로 merge conflicts resolve

---
+++ build/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Iwp1UrdJ8I2TDz+O31F/DyruOL4acGCGj4tsOkrF+ro=\"\n}"
---
+++ build/static/chunks/src_app_page_tsx_0c58a0._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_0c58a0._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_0c58a0._.js",
+  "chunks": [
+    "static/chunks/src_fed553._.js",
+    "static/chunks/node_modules_next_f346a4._.js",
+    "static/chunks/node_modules_@faker-js_faker_dist_3f55fe._.js",
+    "static/chunks/node_modules_react-icons_bi_index_mjs_cba0e8._.js"
+  ],
+  "source": "dynamic"
+});

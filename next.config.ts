c95fdb00c5400ef5c56d6a18fe973b866d31b105
KIMJEONGHYEON <68999618+CodingKirby@users.noreply.github.com>
2025-01-22 22:36:01 +0900

    fix: export default 만 사용하도록 변경

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ const nextConfig: NextConfig = {
 				hostname: 'imgnews.pstatic.net',
 				pathname: '/**',
 			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				pathname: '/**',
+			},
 		],
 	},
 	compiler: {

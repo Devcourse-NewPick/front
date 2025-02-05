c27d571d4a6711ea200e66e287f84343717b1e49
KIMJEONGHYEON <68999618+CodingKirby@users.noreply.github.com>
2025-02-05 22:15:21 +0900

    remove: api route 제거

--- src/app/(api)/api/auth/google/route.ts
+++
@@ -1,6 +0,0 @@
-import API_ENDPOINTS from '@/constants/api';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	res.redirect(API_ENDPOINTS.AUTH.CALLBACK());
-}

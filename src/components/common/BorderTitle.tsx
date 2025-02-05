643fcc6d27c2fb4c4cdcc85be942d47c1a176071
KIMJEONGHYEON <68999618+CodingKirby@users.noreply.github.com>
2025-02-05 09:42:11 +0900

    feat: About 페이지의 제목 공통 컴포넌트로 분리

---
+++ src/components/common/BorderTitle.tsx
@@ -0,0 +1,42 @@
+import Title from '@/components/common/Title';
+import { styled } from 'styled-components';
+import { ColorKey } from '@/styles/theme';
+
+interface Props {
+	className?: string;
+	text: string;
+	color?: ColorKey;
+}
+
+export default function BorderTitle({ className, text, color }: Props) {
+	return (
+		<StyledBorderTitle $color={color} className={className}>
+			<Title size="small" weight="semiBold" className="title">
+				{text}
+			</Title>
+			<div className="bar" />
+		</StyledBorderTitle>
+	);
+}
+
+interface StyledProps {
+	$color?: ColorKey;
+}
+
+const StyledBorderTitle = styled.div<StyledProps>`
+	display: flex;
+	align-items: center;
+	margin-bottom: 3rem;
+
+	.title {
+		width: 20rem;
+		color: ${({ theme, $color }) => ($color ? theme.color[$color] : theme.color.text)};
+	}
+
+	.bar {
+		border-bottom: 1px solid ${({ theme, $color }) => ($color ? theme.color[$color] : theme.color.text)};
+		color: ${({ theme, $color }) => ($color ? theme.color[$color] : theme.color.text)};
+		width: 100%;
+		height: fit-content;
+	}
+`;
